c5c0ed1fc167c1e0e050a3bf9b951de69cee0b14
504 <504@DESKTOP-ULF436B>
2023-01-16 17:24:27 +0900

    Day 12 방문록 수정

--- frontend/public/JS/3.객체/2.방문록.js
+++ frontend/public/JS/3.객체/2.방문록.js
@@ -11,16 +11,39 @@ function 작성(){ // onclick = "작성()"
 		console.log('3.작성자 : ' + 작성자)
 	let 내용 = document.querySelector('.내용').value;
 		console.log('3.내용 : ' + 내용) 
-	// 2. 입력받은 2개의 데이터를 하나로 객체화
-	let board = { writer : 작성자 , content : 내용 }
+	let 비밀번호 = document.querySelector('.비밀번호').value;	//1번
+	
+	// 2. 입력받은 여러개의 데이터를 하나로 객체화
+	let board = {writer : 작성자 
+				,content : 내용 
+				,password : 비밀번호							//2번 
+				}
 		console.log( board )
 	//3. 객체를 배열에 저장 [ 여러개 객체를 저장 ]
 	boardArray.push( board )
+	출력()
 		console.log( boardArray )
 
+}//f e	
+
+// 2. 해당 인덱스의 객체[게시물]를 1개 삭제 함수
+function 삭제( i ){	// i : 내가 삭제할 게시물의 인덱스
+	// 1. [비밀번호 검증] 비밀번호 입력받는다.
+	let 비밀번호 = prompt('게시물 비밀번호 입력 :')
+	
+	// 2. 입력받은 비밀번호와 내가 선택한 게시물의 비밀번호 같으면
+	if(비밀번호 == boardArray[i].password ){
+		boardArray.splice(i,1);// i번째 인덱스 = 객체 삭제
+		alert('게시물 삭제 성공'); 출력();
+	} else { alert('패스워드가 다릅니다. 삭제실패') }
+}
+
+
+// 3. 게시물 출력함수	[1. 작성 성공시 2. 삭제 성공시]
+function 출력(){
 	//4. 배열내 객체 출력
 		// 1. 테이블의 제목행 만들기
-	let html = `<tr> <th>번호</th> <th>내용</th> <th>작성자</th> </tr>`
+	let html = `<tr> <th>번호</th> <th>내용</th> <th>작성자</th> <th>비고</th> </tr>`
 		// 2. 반복문 이용한 배열 내 모든 요소를 행 만들기[누적]
 			// 1. 반복 이용해서 배열 내 객체 하나씩 호출 : boardArray[i]
 			// 2. 객체 내 속성호출					 : boardArray[i].속성명
@@ -29,8 +52,42 @@ function 작성(){ // onclick = "작성()"
 						<td> ${ i+1 } </td>
 						<td> ${ boardArray[i].content }</td>
 						<td> ${ boardArray[i].writer }</td>
-					</tr>`;		
+						<td> <button onclick="삭제( ${i} )" > 삭제 </button> 
+							 <button onclick="수정( ${i} )" > 수정 </button>
+						</td> 
+					</tr>`;		// 내용이나 작성자는 같은 사람이 또 작성 할 수도 있어서 index로 누굴 삭제할지 구분한다.
 		}// for end
 			// 3. 반복문 종료시 누적되 html 해당 table에 출력
 			document.querySelector('.게시물테이블').innerHTML = html;
-}//f e	
+}
+
+// 4. 해당 인덱스의 객체[게시물]를 1개 내용 수정 함수
+function 수정( i ){
+	// 1. 비밀번호 검증
+	let 비밀번호 = prompt('수정할 게시물 비밀번호 입력 : ')
+	// 2. 입력받은 비밀번호와 게시물의 비밀번호가 동일하면
+	if(비밀번호 == boardArray[i].password){
+		// 3. 수정할 내용 입력받아
+		let 수정본 = prompt('수정할 내용 입력 : ')
+		// 4. 선택한 게시물의 속성의 값 수정
+		boardArray[i].content = 수정본;
+		alert('수정 성공');
+		//5. 화면 새로고침/업데이트
+		출력();
+	}else{// 같지않으면
+		alert('비밀번호가 일치하지 않습니다. 수정실패')
+	}
+}
+
+// boardArray				: 얘는 배열 이지만
+// boardArray[i]			: 배열내 i번째 요소 --> 객체 (위에서 배열에 객체를 push했으므로)
+// boardArray[i].content	: 객체.속성명
+
+
+
+
+
+
+
+
+
