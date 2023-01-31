47e09903344fe6503d20358cbc1718c88b8071cb
504 <504@DESKTOP-ULF436B>
2023-01-31 12:08:39 +0900

    Day 20 캘린더 일정추가

--- jsp/src/main/webapp/js/member/calender.js
+++ jsp/src/main/webapp/js/member/calender.js
@@ -1,3 +1,9 @@
+/* DB */
+let contents = [	// 일정목록
+	{ date : '20230101' , content : '새해맞이 여행'  },
+	{ date : '20230103' , content : '친구 만나기'  },
+	{ date : '20230101' , content : '새해맞이 쇼핑새해맞이 쇼핑새해맞이 쇼핑새해맞이 쇼핑'  }
+]
 
 /*----------------전역변수 : 모든 함수에서 공용으로 사용되는 메모리[변수]---------------*/
 // 1. js 열렸을때 현재 연도와 월을 구해서 변수에 저장
@@ -16,24 +22,50 @@ document.querySelector('.top_date').innerHTML = `${year}년 ${month}월`;
 				<div class="day weekday">토</div>`
 				
 	// * 현재 캘린더 설정된 날짜의 마지막 일 구하기
-	let lastday = new Date(year , month , 0).getDate();console.log(' 현재 캘린더 마지막 날짜:' + lastday);			
+	let lastday = new Date(year , month , 0).getDate();console.log(' 현재 캘린더 마지막 날짜:' + lastday);		// 중요
 	// *  현재 캘린더 설정된 날짜의 1일 시작 요일 구하기
-	let weekday = new Date( year , month-1 , 1 ).getDay();console.log('현재 캘린더 시작요일 : ' +weekday );
+	let weekday = new Date(year , month-1 , 1 ).getDay();console.log('현재 캘린더 시작일 요일 : ' +weekday );	// 중요
 	
 	// * 2. 시작 요일전에 공백 만들기
 	for(let b = 1 ; b<=weekday ; b++ ){
 		html +=`<div class="day"></div>`	
-	}
+	}// for e
 				
 	// * 1. 일 만들기	[ 1 ~ 마지막 일[new Date(year , month , 0).getDate()] 까지]
 	for(let day= 1 ; day<=lastday; day++){
-		html +=`<div class="day"> ${ day } </div>`		
+		
+		// 4. 1일 ~ 마지막 일 날짜 확인
+		let date = new Date( year , month-1 , day );//console.log(date);
+		let fdate = date_format(date); //console.log(fdate)
+		
+		html +=`<div class="day"> ${ day } ${contents_print( fdate )} </div>`		
 	} // for e
 				
-	// 4. html 마크업 출력
+	// 3. html 마크업 출력
 	document.querySelector('.cal_day').innerHTML=html;
 
 }// f e
+
+// 5. 일정 출력 함수
+function contents_print( fdate ){ console.log(fdate)
+		let html = ``
+	contents.forEach((o)=>{// 일정목록 반복문
+		if(fdate==o.date){// 만약에 인수로 전달된 날짜와 일정목록에서 동일한 날짜가 존재하면
+			html += `<div class="content">${o.content}</div>`			
+		}	
+	})	// for end
+	return html;
+}
+
+// 4. 날짜 포멧 함수 [인수 : 날짜 -----로직[포멧]-----> 반환 : 변경된 날짜 형식]
+function date_format( date ){	
+	let d_year = date.getFullYear(); 
+			// 만약에 월/요일이 한자리수(<=9) 이면 앞에 0 붙이기
+	let d_month = (date.getMonth()+1) <=9 ? '0'+(date.getMonth()+1) :(date.getMonth()+1) ;
+	let d_day = date.getDate()<=9  ? '0' + date.getDate() : date.getDate();
+	return `${d_year}${d_month}${d_day}`
+}
+
 // 3. 이전달 다음달 버튼 클릭 이벤트에 따른 연도와 월이 변경
 document.querySelector('.previousbtn').addEventListener( 'click' , (e)=>{console.log('이전달 버튼 클릭')
 	// 1. 월 1차감 했을경우 만약에 0이면 연도 1차감 월 12설정
@@ -64,11 +96,13 @@ document.querySelector('.nextbtn').addEventListener( 'click' , (e)=>{console.log
 
 
 
+
 /*
 	new Date()	클래스	날짜/시간 관련된 클래스
-			1. let date = new Date : 현재 날짜/시간 객체
+			1. let date = new Date 					: 현재 날짜/시간 객체
 			2. let date = new Date(2023 , 01 , 31 )	: 사용자 정의 날짜/시간 객체
-			3. let date = new Date(연도 , 월 , 2)		: 사용자 정의 연도와 월 날짜에 따른 마지막 일 날짜
+			3. let date = new Date(연도 , 월+1 , 0)	: 사용자 정의 연도와 월 날짜에 따른 마지막 일 날짜
+			3. let date = new Date(연도 , 월 , 1)		: 사용자 정의 연도와 월 날짜에 따른 1일 날짜
 				1.	.getFullYear()	: 연도
 				2.	.getMonth()		: 월	[ 0 ~ 11   0:1월 ~ 11:12월]
 				3.	.getDate()		: 일
@@ -80,4 +114,8 @@ console.log('연도:' + date.getFullYear());
 console.log('월 :'+date.getMonth());
 console.log('일 :' + date.getDate());
 console.log('요일 :' + date.getDay());
+
+	`백틱
+		` 문자열 ${ js작성할 수 있는 구역 }` 즉 함수 연산 상수 다 들갈 수 있음
+
 */
