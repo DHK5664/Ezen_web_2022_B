8c560c867f1d4d85cfe9866cc5e95d38607a3e10
504 <504@DESKTOP-RFOCEJA>
2023-02-09 12:25:41 +0900

    Day 27 객체 필드생성 및 실행

--- backend/src/Day07/Ex1_회원시스템_배열.java
+++ backend/src/Day07/Ex1_회원시스템_배열.java
@@ -12,12 +12,12 @@ public class Ex1_회원시스템_배열 {// c e
 		while(true) {// 무한루프 [종료조건 : 없음]
 			
 			// 2. 배열내 데이터 출력
-			System.out.println("회원명\t전화번호");
+			System.out.println("번호\t회원명\t전화번호");
 			for(int i = 0 ; i<memberList.length ; i++) {
 				if(memberList[i]!=null){	// null 데이터없 -> split 불가능
 					String[] member = memberList[i].split(",");	// "유재석" , "010-4444-4444" , null , null -> split(",")
 					// member = { "유재석" , "010-4444-4444"}
-					System.out.println(member[0]+"\t"+member[1]);
+					System.out.println(i+"\t"+member[0]+"\t"+member[1]);
 				}
 			}
 			System.out.print("1.회원등록 2.회원삭제 : ");			
@@ -38,10 +38,45 @@ public class Ex1_회원시스템_배열 {// c e
 				}// for end
 				
 			}	// 1 if end
-			else if(ch==2) {} // 2. 2입력했을때
+			else if(ch==2) {
+				
+				System.out.println("삭제할 번호/인덱스 : ");
+				int no = scanner.nextInt();
+				// 3. 배열 내 데이터 삭제 // splice 기능 [배열 내 null 만들기]
+				memberList[no] = null;
+				// * 삭제 후 빈자리 채우기 [ 삭제된 인덱스 뒤로 한칸씩 당기기 ]
+				for(int i = no ; i<memberList.length ; i++) {	// no를 넣은 이유는 삭제된 자리 앞쪽은 가만히 있어도 되고 뒤에만 앞으로 당기면 되니까
+						if( i+1 == memberList.length) {// 마지막인덱스 이면
+							memberList[i] = null; break;	// 마지막인덱스 null 넣고 나가기
+						}
+						memberList[i] = memberList[i+1];
+						if( memberList[i+1] == null ) break;	// 다음 인덱스가 null이면 종료
+				}
+				
+			} // 2. 2입력했을때
 			else {}
 			
 		} // while end
 		
 	}// m e
 }// c s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
