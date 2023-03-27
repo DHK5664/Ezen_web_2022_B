3350772a0afa6ee7c5ecd8648eefebf05cd5e650
504 <504@DESKTOP-LUNAMRA>
2023-03-27 12:29:32 +0900

    Day58 productDto[ 파일명목록,작성자] 추가

--- jspweb/src/main/java/controller/product/Productinfo.java
+++ jspweb/src/main/java/controller/product/Productinfo.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
+import model.dao.MemberDao;
 import model.dao.ProductDao;
 import model.dto.ProductDto;
 
@@ -69,6 +71,7 @@ public class Productinfo extends HttpServlet {
 		DiskFileItemFactory 파일저장소 = new DiskFileItemFactory();
 		파일저장소.setRepository(저장경로객체);			// 파일저장소 위치 대입
 		파일저장소.setSizeThreshold(1024*1024*10);	// 파일저장소에 저장할 수 있는 최대 용량 범위
+		파일저장소.setDefaultCharset("UTF-8");		// 파일저장소 한글타입 인코딩
 		
 		// 4. 파일업로드 객체
 		ServletFileUpload 파일업로드객체 = new ServletFileUpload(파일저장소);
@@ -77,12 +80,48 @@ public class Productinfo extends HttpServlet {
 		try {
 			// 5. 매개변수 요청해서 리스트에 담기 [ 무조건 예외처리 발생 ]
 			List<FileItem> 파일아이템목록 = 파일업로드객체.parseRequest(request);
+			List<String> 일반필드목록 = new ArrayList<>();
+			List<String> 파일필드목록 = new ArrayList<>();
 			// 6.
 			for(FileItem item :파일아이템목록) {	// 요청된 모든 매개변수들을 반복문 돌려서 확인
-				System.out.println("필드명 :" +item.getFieldName() );		// 매개변수 명 확인
-				System.out.println("필드내 첨부된 파일명" +item.getName() );	// 매개변수가 파일일경우 파일명 확인
+				
+				if(item.isFormField()) {
+					// .isFormField() : 첨부파일 아니면 true 첨부파일이면 false
+					System.out.println("[첨부파일 아닌 필드명: " + item.getFieldName());
+					System.out.println("[첨부파일 아닌 필드의 값: " + item.getString());
+					일반필드목록.add(item.getString()); // 입력받은 값을 요청해서 리스트 저장
+				}else {
+					System.out.println("[첨부파일 인 필드명: " + item.getFieldName());
+					System.out.println("[첨부파일 인 필드의 파일명: " + item.getName());
+					
+					// 9. 첨부파일 이름 식별이름 변경	
+						// 1. 파일명에 공백이 존재하면 -로 변경 [.replaceAll("기존문자","새로운문자") 문자열 치환함수 ]
+						// 2. UUID : 범용 고유 식별자 [중복이 없는 식별자 만들기]
+						// 최종 식별 파일명 : UUID 파일명
+					String filename = UUID.randomUUID() +" "+ (item.getName().replaceAll(" ", "-"));	// 파일명에 띄어쓰기 있으면 자르기가 안대서 없앰
+					
+					파일필드목록.add(filename);	// 첨부된 파일의 이름을 요청해서 리스트 저장
+					
+					// 7. 저장할 경로 +/+ 파일명 의 파일을 객체화
+					File 업로드할파일경로 = new File(경로+"/"+filename);					
+					// 8. 해당 경로에 item 업로드하기
+					item.write(업로드할파일경로);
+				}
+				
 			}
-		}catch (Exception e) {}
+			//
+			System.out.println(일반필드목록.toString()); 
+			System.out.println(파일필드목록.toString());
+			
+			int mno = MemberDao.getInstance().getMno((String)request.getSession().getAttribute("login"));
+			
+			ProductDto dto = new ProductDto(
+					일반필드목록.get(0), 일반필드목록.get(1), Integer.parseInt(일반필드목록.get(2)),
+					일반필드목록.get(3), 일반필드목록.get(4), mno, 파일필드목록);
+			
+		}catch (Exception e) {System.out.println("파일저장 실패");}
+		
+		
 		
 		// ---------------------- cos.jar 사용시 ---------------------- //
 		
