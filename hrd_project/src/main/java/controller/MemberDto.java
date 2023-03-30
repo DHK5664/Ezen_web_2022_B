609474eaad184abb021c7d17b424061d19ce2175
504 <504@DESKTOP-LUNAMRA>
2023-03-30 15:13:56 +0900

    Day61 매출출력 / 기출문제1_최종본

--- hrd_project/src/main/java/controller/MemberDto.java
+++ hrd_project/src/main/java/controller/MemberDto.java
@@ -9,10 +9,22 @@ public class MemberDto {
 	private String grade;
 	private int city;
 	
+	// 추가
+	private int psum; // 총매출
+	
 	public MemberDto() {
 		// TODO Auto-generated constructor stub
 	}
-
+	// 통계용 생성자
+	public MemberDto(int custno, String custname, String grade, int psum) {
+		super();
+		this.custno = custno;
+		this.custname = custname;
+		this.grade = grade;
+		this.psum = psum;
+	}
+	
+	
 	public MemberDto(int custno, String custname, String phone, String address, String joindate, String grade,
 			int city) {
 		super();
@@ -24,68 +36,74 @@ public class MemberDto {
 		this.grade = grade;
 		this.city = city;
 	}
-
+	public MemberDto(int custno, String custname, String phone, String address, String joindate, String grade, int city,
+			int psum) {
+		super();
+		this.custno = custno;
+		this.custname = custname;
+		this.phone = phone;
+		this.address = address;
+		this.joindate = joindate;
+		this.grade = grade;
+		this.city = city;
+		this.psum = psum;
+	}
+	
 	@Override
 	public String toString() {
 		return "MemberDto [custno=" + custno + ", custname=" + custname + ", phone=" + phone + ", address=" + address
-				+ ", joindate=" + joindate + ", grade=" + grade + ", city=" + city + "]";
+				+ ", joindate=" + joindate + ", grade=" + grade + ", city=" + city + ", psum=" + psum + "]";
 	}
-
 	public int getCustno() {
 		return custno;
 	}
-
 	public void setCustno(int custno) {
 		this.custno = custno;
 	}
-
 	public String getCustname() {
 		return custname;
 	}
-
 	public void setCustname(String custname) {
 		this.custname = custname;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public String getJoindate() {
 		return joindate;
 	}
-
 	public void setJoindate(String joindate) {
 		this.joindate = joindate;
 	}
-
 	public String getGrade() {
 		return grade;
 	}
-
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
-
 	public int getCity() {
 		return city;
 	}
-
 	public void setCity(int city) {
 		this.city = city;
 	}
+	public int getPsum() {
+		return psum;
+	}
+	public void setPsum(int psum) {
+		this.psum = psum;
+	}
+
+	
 
 	
 	
