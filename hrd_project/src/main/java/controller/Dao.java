9a642802991991dd95e7a54163f3f6accf21438b
504 <504@DESKTOP-LUNAMRA>
2023-03-30 13:38:34 +0900

    Day 61 회원 출력

--- hrd_project/src/main/java/controller/Dao.java
+++ hrd_project/src/main/java/controller/Dao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Dao {
 		
@@ -24,7 +25,6 @@ public class Dao {
 			con = getConnection();
 		}catch (Exception e) {System.out.println(e);}
 	}
-	
 	// 1. 마지막 회원번호+1 가져오기
 	public int getcustno() {
 		String sql = "select max(custno)+1 from member_tbl_02";
@@ -42,13 +42,34 @@ public class Dao {
 		try {
 			ps = con.prepareStatement(sql);
 			ps.setString(1, dto.getCustname());
-			ps.setString(1, dto.getPhone());
-			ps.setString(1, dto.getAddress());
-			ps.setString(1, dto.getJoindate());
-			ps.setString(1, dto.getGrade());
-			ps.setInt(1, dto.getCity());
+			ps.setString(2, dto.getPhone());
+			ps.setString(3, dto.getAddress());
+			ps.setString(4, dto.getJoindate());
+			ps.setString(5, dto.getGrade());
+			ps.setInt(6, dto.getCity());
 			ps.executeUpdate(); return true;
-		}catch (Exception e) {} return false;
+		}catch (Exception e) {System.out.println(e);} return false;
 	}
+	
+	// 3. 모든 회원 목록 출력
+	public ArrayList<MemberDto> getmemberlist(){
+		ArrayList<MemberDto> list = new ArrayList<>();
+		String sql="select "
+				+ "	custno , custname , phone , address , joindate , "
+				+ "    if(grade ='A' , 'VIP' , if(grade='B' , '일반' , '직원') ) as g , "
+				+ "	city "
+				+ "from member_tbl_02;";
+		try {
+			ps = con.prepareStatement(sql);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				list.add(new MemberDto(rs.getInt(1),rs.getString(2),rs.getString(3),
+						rs.getString(4),rs.getString(5),rs.getString(6),
+						rs.getInt(7)
+						));
+			}			
+		}catch (Exception e) {System.out.println(e);} return list;
+	}
+	
 }
 
